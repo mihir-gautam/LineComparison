013af7050dd142eac1cc5abd17efe0f754036321
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-09-29 20:04:32 +0530

    [Mihir] Add Computation of line length

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace LineComparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison computation program!");
+            int x1, y1, x2, y2;
+            Console.WriteLine("Input the X1: ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the Y1: ");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the X2: ");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the Y2: ");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            double lineLength = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+            Console.WriteLine("Length of the first line = " + lineLength);
         }
     }
 }
