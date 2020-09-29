ef39cd592fb6474f471a699d1f4f616827272257
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-09-29 20:40:18 +0530

    [Mihir] Refactor Checked the equality of two lines

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Net;
+using System.Numerics;
 
 namespace LineComparison
 {
     class Program
     {
-        static void Main(string[] args)
+        static double lengthOfLine()
         {
             int x1, y1, x2, y2;
             Console.WriteLine("Input the X1: ");
@@ -15,9 +17,27 @@ namespace LineComparison
             x2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Input the Y2: ");
             y2 = Convert.ToInt32(Console.ReadLine());
-
-            double lineLength = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
-            Console.WriteLine("Length of the first line = " + lineLength);
+            
+            double lineLength = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            return lineLength;
         }
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison computation program");
+            Console.WriteLine("Enter the coordinates of line 1");
+            double LengthOfLine1 = lengthOfLine();
+            Console.WriteLine("Enter the coordinates of line 2");
+            double LengthOfLine2 = lengthOfLine();
+
+            if(LengthOfLine1 == LengthOfLine2)
+            {
+                Console.WriteLine("Both lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not equal!");
+            }
+        }
+
     }
 }
