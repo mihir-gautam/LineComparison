46887a95dfffeb30bba191981e7dd95aa46e2fc1
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-09-29 20:54:26 +0530

    [Mihir] Refactor Comparison of lengths

--- Program.cs
+++ Program.cs
@@ -8,15 +8,14 @@ namespace LineComparison
     {
         static double lengthOfLine()
         {
-            int x1, y1, x2, y2;
             Console.WriteLine("Input the X1: ");
-            x1 = Convert.ToInt32(Console.ReadLine());
+            int x1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Input the Y1: ");
-            y1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Input the X2: ");
-            x2 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Input the Y2: ");
-            y2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
             
             double lineLength = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
             return lineLength;
@@ -24,18 +23,26 @@ namespace LineComparison
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to Line Comparison computation program");
+
             Console.WriteLine("Enter the coordinates of line 1");
             double LengthOfLine1 = lengthOfLine();
+            Console.WriteLine("Length of line 1 : " + LengthOfLine1);
+
             Console.WriteLine("Enter the coordinates of line 2");
             double LengthOfLine2 = lengthOfLine();
+            Console.WriteLine("Length of line 2 : " + LengthOfLine2);
 
             if(LengthOfLine1 == LengthOfLine2)
             {
-                Console.WriteLine("Both lines are equal");
+                Console.WriteLine("Both lines are equal in length");
+            }
+            else if (LengthOfLine1 > LengthOfLine2)
+            {
+                Console.WriteLine("Line 1 is greater than Line 2");
             }
             else
             {
-                Console.WriteLine("Lines are not equal!");
+                Console.WriteLine("Line 1 is smaller than Line 2");
             }
         }
 
